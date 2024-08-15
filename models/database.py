26d649cac9be94902554acb888290b068f444e03
LouisCR04 <louiskariuki04@gmail.com>
2024-08-15 23:32:38 +0300

    Modified mongoclient to mongoengine DRM

--- models/database.py
+++ models/database.py
@@ -11,7 +11,7 @@ class Commodity(Document):
     factors = DictField() # Store attributes like capacity and location
     available = BooleanField(default=True)
     meta = {
-        'db_alias': 'core'
+        'db_alias': 'core' # Which db conn to use
         'collection':'commodity'
     }
 
@@ -20,7 +20,7 @@ class Entity(Document):
     type = StringField(required=True, max_length=50) #e.g. student/camp
     name = StringField(required=True, max_length=100) #e.g. Aizen/Gaza
     description = StringField(max_length=500) #optional
-    factors = DictField() # Store attributes like financial_need
+    factors = DictField() #Factors like financial_need to help calc weights
     available = BooleanField(default=True)
     meta = meta = {
         'db_alias': 'core'

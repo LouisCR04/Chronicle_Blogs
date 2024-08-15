5ca5b2dcdae4e2f6d74547cbaf6f7f3c29be4eed
LouisCR04 <louiskariuki04@gmail.com>
2024-08-15 22:43:55 +0300

    Commodity & Entity Collections added

--- models/database.py
+++ models/database.py
@@ -10,3 +10,19 @@ class Commodity(Document):
     description = StringField(max_length=500) #optional
     factors = DictField() # Store attributes like capacity and location
     available = BooleanField(default=True)
+    meta = {
+        'db_alias': 'core'
+        'collection':'commodity'
+    }
+
+
+class Entity(Document):
+    type = StringField(required=True, max_length=50) #e.g. student/camp
+    name = StringField(required=True, max_length=100) #e.g. Aizen/Gaza
+    description = StringField(max_length=500) #optional
+    factors = DictField() # Store attributes like financial_need
+    available = BooleanField(default=True)
+    meta = meta = {
+        'db_alias': 'core'
+        'collection':'entity'
+    }
--- models/main.py
+++ models/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Initializes the database
 """
@@ -6,3 +7,6 @@ import mongoengine
 
 def global_init():
     mongoengine.register_connection(alias='core', name='allocations')
+
+if __name__ == "__main__":
+    global_init()

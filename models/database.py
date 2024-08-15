5489a6a8afb64a621ba1a6e2c169d61ca4e26ae5
LouisCR04 <louiskariuki04@gmail.com>
2024-08-16 00:59:56 +0300

    Weighted algorithm modified

--- models/database.py
+++ models/database.py
@@ -8,7 +8,7 @@ class Commodity(Document):
     type = StringField(required=True, max_length=50) #e.g. room/finances
     name = StringField(required=True, max_length=100) #e.g. room101
     description = StringField(max_length=500) #optional
-    factors = DictField() # Store attributes like capacity and location
+    factors = DictField()
     available = BooleanField(default=True)
     meta = {
         'db_alias': 'core' # Which db conn to use

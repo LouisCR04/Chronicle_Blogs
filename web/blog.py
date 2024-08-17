63a9a9ac96810b66f24d14c414277370c867cce0
LouisCR04 <louiskariuki04@gmail.com>
2024-08-17 17:58:25 +0300

    User registration and login added

--- web/blog.py
+++ web/blog.py
@@ -2,8 +2,15 @@
 """
 Main Flask Router
 """
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegForm, LoginForm
 app = Flask(__name__)
+
+"""
+import os
+SECRET_KEY = os.urandom(32)
+"""
+app.config['SECRET_KEY'] = '06a0233352eacd9d1c3133047eef7c10'
 
 posts = [
     {
@@ -28,6 +35,20 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title='About Seiretei')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegForm()
+
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", 'success')
+        return redirect(url_for('home'))
+    return render_template("reg.html", title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title='login', form=form)
 
 
 if __name__ == "__main__":

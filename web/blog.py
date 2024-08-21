7382d1fd330af1d6c47c05d9c4c9e9ccfd81bb03
LouisCR04 <louiskariuki04@gmail.com>
2024-08-21 23:52:35 +0300

    Cleaned the frontend

--- web/blog.py
+++ web/blog.py
@@ -39,8 +39,7 @@ def load_user(user_id):
 @app.route("/home")
 def home():
     posts = Post.objects.all()
-    p_img = url_for('static', filename='post_pics/default.jpg')
-    return render_template("home.html", posts=posts, post_image=p_img)
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
